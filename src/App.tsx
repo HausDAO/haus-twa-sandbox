528586d137e06bbcea0ea62b03145788f1a1ac0b
skuhlmann <samkuhlmann@gmail.com>
2023-11-09 11:17:16 -0700

    adds proposals

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,74 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useState } from "react";
+import hausLogo from "./assets/icon-512x512.png";
 import "./App.css";
 
-import WebApp from "@twa-dev/sdk";
+// import WebApp from "@twa-dev/sdk";
+import { useDaoProposals } from "@daohaus/moloch-v3-hooks";
+import { MolochV3Proposal } from "@daohaus/moloch-v3-data";
+import { BeatLoader } from "react-spinners";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [proposal, setProposal] = useState<MolochV3Proposal | undefined>();
+  const { proposals, isLoading } = useDaoProposals({
+    daoChain: "0xa",
+    daoId: "0xf5d6b637a9185707f52d40d452956ca49018247a",
+    paging: {
+      pageSize: 1,
+      offset: count,
+    },
+  });
+
+  useEffect(() => {
+    console.log("proposals", proposals);
+
+    setProposal(proposals[0]);
+  }, [proposals]);
+
+  const displayProposal = () => {
+    setCount((count) => count + 1);
+    setProposal(proposals[0]);
+  };
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://daohaus.club/" target="_blank">
+          <img src={hausLogo} className="logo" alt="Haus logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>PublicHAUS DAO</h1>
+      <h6>Latest Proposal</h6>
+      {isLoading && <BeatLoader color="#646cff" />}
+      {proposal && (
+        <div className="proposal">
+          <a
+            href={`https://admin.daohaus.club/#/molochV3/0xa/0xf5d6b637a9185707f52d40d452956ca49018247a/proposal/${proposal.proposalId}`}
+            target="_blank"
+          >
+            <h3>{proposal.title}</h3>
+            <p className="status">Status: {proposal.status}</p>
+            <div className="votes">
+              <p>{`Yes: ${(
+                Number(proposal.yesBalance) /
+                10 ** 18
+              ).toFixed()}`}</p>
+              <p>{`No: ${(
+                Number(proposal.noBalance) /
+                10 ** 18
+              ).toFixed()}`}</p>
+            </div>
+          </a>
+        </div>
+      )}
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={displayProposal}>Show me the next proposal</button>
       </div>
-      <div className="card">
-        <button
-          onClick={() =>
-            WebApp.showAlert(`Hello World! Current count is ${count}`)
-          }
-        >
-          Show Alert
+      {/* <div className="card">
+        <button onClick={() => WebApp.showAlert(`I can open alerts!`)}>
+          YO!
         </button>
-      </div>
+      </div> */}
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import WebApp from "@twa-dev/sdk";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 import App from "./App.tsx";
 import "./index.css";
 
 WebApp.ready();
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
